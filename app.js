57cca13ba4662b7888f350730eff813e9f7d1835
tswack75 <32391798+tswack75@users.noreply.github.com>
2026-04-07 08:07:04 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "v4.7.3";
+const APP_VERSION = "v4.7.4";
 const STORAGE_KEY = "health-quest-v3";
 const LEGACY_KEYS = ["health-quest-v2", "health-quest-v1"];
 const FOOD_SCORING_UPDATE_DATE = "2026-04-06";
